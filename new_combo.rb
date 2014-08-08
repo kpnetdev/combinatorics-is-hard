8aa64cbd73b5e640d28076f293887b0ef2aa3d9e
kpnetdev <kelley.puckett@gmail.com>
2014-08-08 10:24:06 -1000

    * first push to github

---
+++ new_combo.rb
@@ -0,0 +1,239 @@
+require 'pry'
+
+class Combo
+  attr_accessor :yet_to_pair, :cohort, :master_combos, :sample_size
+
+  def initialize(cohort, students_per_team, opts={})
+    @cohort = cohort
+    # @score = nil
+    @per_team = students_per_team
+    @sample_size = opts[:sample] || 3
+    @bind = opts[:bind]
+    @yet_to_pair = Hash.new     # key is student, value is array of students they've yet to pair with
+    @cohort.each do |student|
+      @yet_to_pair[student] = @cohort - [student]
+    end
+  end
+
+  def perfect_score
+    @cohort.length * (@per_team - 1)
+  end
+
+  def refresh_score
+    @score = @yet_to_pair.values.inject(:+).count
+  end
+
+  def easy_first_pass
+    @prev = @score
+    students = @cohort.dup
+    teams_list = []
+    (@cohort.length / @per_team).times do
+      team = students.slice!((0...@per_team))
+      process_teams(team)
+      teams_list << team
+    end
+    refresh_score
+    score(1, teams_list)
+  end
+
+  def run
+    counter = 1
+    easy_first_pass
+    until @score == 0 do
+      counter += 1
+      @prev = @score
+      top_groupings = generate_potential_pairings
+      # puts "done generating"
+      # puts highest_rated(top_groupings).count
+      rated_groupings = top_groupings.sort_by {|group| rate_pairings(group)}
+      # puts top_rated(rated_groupings).count
+      best_group = rated_groupings.last
+      process_group(best_group)
+      refresh_score
+      score(counter, best_group)
+    end
+    puts "#{counter} iterations"
+  end
+
+  def process_group(group)
+    group.each {|team| process_teams(team)}
+  end
+
+
+  def new_iterative(master_list)
+    new_list = []
+    if master_list.empty?
+      get_top_three(@cohort).each {|group| new_list << [group]}
+    else
+      master_list.each {|solution| new_list += next_three(solution)}
+    end
+    new_list
+  end
+
+  def look_ahead(best_group)
+    ytp_org = @yet_to_pair.dup
+    process_group(best_group)
+    top_groupings = generate_potential_pairings
+    highest_score = top_score(top_groupings)
+    highest_rated = top_groupings.select {|group| rate_pairings(group) == highest_score}
+    @yet_to_pair = ytp_org
+    highest_score
+  end
+
+  def top_score(groups)
+    sorted = groups.sort_by {|group| rate_pairings(group)}
+    rate_pairings(sorted.last)
+  end
+
+  def highest_rated(groups)
+    groups.select {|group| rate_pairings(group) == top_score(groups)}
+  end
+
+  def top_rated(rated_groups)
+    rated = rated_groups.dup
+    top_score = rate_pairings(rated.last)
+    return_list = [rated.pop]
+    # return_list = [rated.pop]
+    return_list << rated.pop while (rate_pairings(return_list.last) == top_score)
+    return_list.pop
+    return_list
+    # binding.pry
+  end
+
+  def generate_potential_pairings
+    master_list = []
+    master_list = new_iterative(master_list) until fully_populated?(master_list)
+    uniq_it(master_list)
+    # master_list
+  end
+
+  def uniq_it(master_list)
+    sorted = master_list.map do |arr|
+      arr.sort_by {|sub| sub.first}
+    end
+    sorted.uniq
+  end
+
+  def rate_pairings(groups)
+    groups.inject(0) {|cumulative_score, group| cumulative_score + prospective_score(group)}
+  end
+
+  def next_three(groups_so_far)
+    return_list = []
+    students = @cohort - groups_so_far.flatten
+    temp_hash = @yet_to_pair.dup
+    groups_so_far.each do |group|
+      temp_hash = process_teams(group, temp_hash)
+    end
+    next_best_three = get_top_three(students, temp_hash)
+    next_best_three.each do |next_team|
+      return_list << groups_so_far + [next_team]
+    end
+    return_list
+  end
+
+  def fully_populated?(group_list)
+    return false if group_list.empty?
+    group_list.last.flatten.count == @cohort.count
+  end
+
+
+  def get_top_three(students, hash_to_score=@yet_to_pair.dup)
+    combos = students.combination(@per_team).to_a
+    sorted_combos = combos.sort_by {|c| prospective_score(c)}
+    top_three = []
+    @sample_size.times do
+      top_three << sorted_combos.pop
+    end
+    top_three.compact
+  end
+
+  # def old_prospective_score(team, back=@yet_to_pair.dup)
+  #   backup_hash = @yet_to_pair.dup
+  #   before_processing_score = score_teams(team, backup_hash)
+  #   backup_hash = process_teams(team, backup_hash)
+  #   after_processing_score = score_teams(team, backup_hash)
+  #   before_processing_score - after_processing_score
+  # end
+
+  def prospective_score(team)
+    team.inject(0) do |sum, boot|
+      other_team_members = team - [boot]
+      common_boots = @yet_to_pair[boot] & other_team_members
+      sum + common_boots.count
+    end
+  end
+
+
+
+  def score_teams(team, hash_to_score=@yet_to_pair)
+    team.inject(0) {|score, boot| score + hash_to_score[boot].length }
+  end
+
+  def process_teams(team, hash_to_process=@yet_to_pair)
+    team.each do |element|
+      others = [team - [element]].flatten
+      others.each do |o|
+        hash_to_process[element] -= [o]
+      end
+    end
+    hash_to_process
+    # @master_combos.delete(team)
+  end
+
+  def students_with_num_matches(number)
+    @yet_to_pair.keys.select {|k| @yet_to_pair[k].length == number}
+  end
+
+  def percentage_off
+    @prev ||= @score
+    diff = @prev - @score
+    binding.pry if @bind
+    ((diff.to_f / perfect_score)  * 100).to_i
+  end
+
+  def score(counter, teams_list)
+    puts "ROUND #{counter}"
+    puts "*" * 10
+    puts "score: #{@score}"
+    puts "percentage of perfect: #{percentage_off}"
+    puts "paired with everybody: #{(students_with_num_matches(0)).length}"
+    # puts "*" * 10
+    puts "missed one pair: #{(students_with_num_matches(1)).length}"
+    puts "missed more than one: #{(@yet_to_pair.keys.select {|k| @yet_to_pair[k].length > 1}).length}"
+    # sample_student(teams_list)
+  end
+
+  def sample_student(teams_list)
+    student = @cohort.first
+    current_team = teams_list.select {|team| team.include?(student)}
+    puts "sample student: #{student}"
+    puts "     team chosen: #{current_team}"
+    puts "     left to pair: #{@yet_to_pair[student]}"
+    puts
+  end
+end
+
+class Round  # Eh. Is 'Round' really what I want? What holds the master hash?
+  attr_reader :score
+
+  def refresh_score!
+  end
+
+end
+
+
+
+def schoolgirls(opts={})
+  schoolgirl_list = %w(A B C D E F G H I J K L M N O)
+  group_size = 3
+  sg = Combo.new(schoolgirl_list, group_size, opts)
+  sg.run
+end
+
+def boots(opts={}) # sample of '3' gives 7 iterations
+  boots_list = %w(A B C D E F G H I J K L M N O P Q R S T U V W X)
+  group_size = opts[:per_team] || 6
+  b = Combo.new(boots_list, group_size, opts)
+  b.run
+end
---
+++ spec/new_combo_spec.rb
@@ -0,0 +1,115 @@
+require './new_combo.rb'
+
+describe Combo do
+
+	before(:each) do
+		@c = Combo.new(%w(A B C D E F), 2)
+	end
+
+	it "initializes the yet_to_pair hash correctly" do
+		expect(@c.yet_to_pair["A"]).to eq(["B", "C", "D", "E", "F"])
+	end
+
+	it "correctly scores a zero-score prospective pairing" do
+		@c.yet_to_pair["A"] = %w(C D E F)
+		@c.yet_to_pair["B"] = %w(C D E F)
+		team = ["A", "B"]
+
+		expect(@c.prospective_score(team)).to eq(0)
+	end
+
+	it "correctly scores a two-score pairing" do
+		@c.yet_to_pair["A"] = ["B"]
+		@c.yet_to_pair["B"] = ["A"]
+		team = ["A", "B"]
+
+		expect(@c.prospective_score(team)).to eq(2)
+	end
+
+	it "initializes the default @sample variable correctly" do
+		expect(@c.sample_size).to eq(3)
+	end
+
+	it "takes a @sample value from opts correctly" do
+		@d = Combo.new(["A", "B"], 2, {sample: 4})
+
+		expect(@d.sample_size).to eq(4)
+	end
+
+	describe "#look_ahead" do
+
+		it "doesn't mess up @yet_to_pair" do
+			pre = @c.yet_to_pair.dup
+			group = [["A", "B"], ["C", "D"], ["E", "F"]]
+			@c.look_ahead(group)
+
+			expect(@c.yet_to_pair).to eq(pre)
+		end
+	end
+
+	describe "#perfect_score" do
+
+		it "gives the top pairing score possible" do
+			expect(@c.perfect_score).to eq(6)
+		end
+
+		it "works for boots too" do
+			b = Combo.new((1..24).to_a, 6)
+			expect(b.perfect_score).to eq(120)
+		end
+	end
+
+	describe "maybe refactor score_teams" do
+		it "works the way I think" do
+			expect(@c.score_teams(["A", "B"])).to eq(10)
+		end
+	end
+
+
+
+	# it "removes the teams from the master combo list" do
+	# 	team = %w(A B)
+	# 	expect(@c.master_combos.include?(team)).to eq(true)
+	# 	@c.process_teams(team)
+	# 	expect(@c.master_combos.include?(team)).to eq(false)
+	# end
+
+	# it "checks if the given list is a full pairing list" do
+	# 	expect(@c.pairing_list_full?(@c.cohort)).to eq(true)
+	# 	expect(@c.pairing_list_full?(["A"])).to eq(false)
+	# end
+
+	# it "gives all the full pass combos for a given first team" do
+	# 	first_team = ["A", "B"]
+	# 	expected_list = [[["A", "B"], ["C", "D"], ["E", "F"]], [["A", "B"], ["C", "E"], ["D", "F"]]]
+	# 	expect(@c.all_week_lineups(first_team)).to eq(expected_list)
+	# end
+
+	# it "finds the common matches correctly" do
+	# 	@d = Combo.new(%w(A B C D E F G H I), 3)
+	# 	preselected = ["A", "B", "C"]
+	# 	@d.yet_to_pair["A"] = ["D", "E", "G"]
+	# 	@d.yet_to_pair["B"] = ["E", "G", "I"]
+	# 	@d.yet_to_pair["C"] = ["Y", "G"]
+
+	# 	expect(@d.common_matches(preselected)).to eq(["G"])
+	# end
+
+	# it "sorts students by yet_to_pair array length" do
+	# 	students = %w(A B C)
+	# 	@c.yet_to_pair["A"] = ["G"]
+	# 	@c.yet_to_pair["B"] = ["J", "K", "L"]
+	# 	@c.yet_to_pair["C"] = ["D", "T"]
+
+	# 	expect(@c.sort_students(students)).to eq(["B", "C", "A"])
+	# end
+
+	# it "sorts unmatches by their incidence in the group" do
+	# 	@c.yet_to_pair["A"] = %w(D F J)
+	# 	@c.yet_to_pair["B"] = %w(D F X)
+	# 	@c.yet_to_pair["C"] = %w(D F J)
+
+	# 	expect(@c.rank_non_matches(%w(A B C))).to eq(["D", "F", "J", "X"])
+
+	# end
+end
